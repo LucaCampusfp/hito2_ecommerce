6fa9d3695c72610e1ba1ab02e24e1ed18216d75d
LucaCampusfp <lucacampusfp@gmail.com>
2025-01-14 17:01:42 +0100

    actualizar

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css';
+import './navbar.css';
 import logo from '../icons/pokemon.svg';
 
 const Navbar = () => {
